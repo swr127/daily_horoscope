d94a84d8564dab8e3cd33bc9b835bdf0ced92078
swr127 <samantha.w.russell@gmail.com>
2020-01-27 09:24:15 -0500

    set up app structure

--- daily_horoscope/src/App.js
+++ daily_horoscope/src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route } from 'react-router-dom'
+import Axios from 'axios'
+import Header from './components/Header'
+import Main from './components/Main'
+import Profile from './components/Profile'
+import Footer from './components/Footer'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Main />
+      <Profile />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
