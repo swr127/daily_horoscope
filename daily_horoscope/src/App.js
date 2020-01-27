3b406a44661e720debdc7bc1ca43d36eeccee3f3
swr127 <samantha.w.russell@gmail.com>
2020-01-27 10:10:42 -0500

    set up API call

--- daily_horoscope/src/App.js
+++ daily_horoscope/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Route } from 'react-router-dom'
 import Axios from 'axios'
 import Header from './components/Header'
@@ -7,15 +7,51 @@ import Profile from './components/Profile'
 import Footer from './components/Footer'
 import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Main />
-      <Profile />
-      <Footer />
-    </div>
-  )
+// const url = 'https://aztro.sameerkumar.website/?sign=aquarius&day=today'
+
+// function App() {
+//   const [sign, setSign] = useState([])
+
+//   const getSign = async () => {
+//     let response = await Axios.get(url)
+//     setSign(response.data)
+//     console.log(response.data)
+//   }
+
+//   useEffect(() => {
+//     getSign()
+//   }, []) 
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      sign: {}
+    }
+  }
+
+  componentDidMount () {
+    const url = 'https://aztro.sameerkumar.website/?sign=aquarius&day=today'
+    fetch(url, {
+      method: 'POST'
+    }).then(response => response.json()).then(sign => {
+      this.setState({sign})
+    })
+  }
+
+  render() {
+    let response = this.state.sign
+    console.log(response)
+  
+    return (
+      <div className="App">
+        <Header />
+        <Main />
+        <Profile />
+        <Footer />
+      </div>
+    )
+  }
 }
 
 export default App
