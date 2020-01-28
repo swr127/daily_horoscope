f608e810ea479e4d40ff3d8ae7e9274f7ba0b7b3
swr127 <samantha.w.russell@gmail.com>
2020-01-28 11:21:31 -0500

    rendering data on Profile

--- daily_horoscope/src/components/Profile.js
+++ daily_horoscope/src/components/Profile.js
@@ -10,25 +10,30 @@ class Profile extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            sign:''
+            sign:'',
+            horoscope:''
         }
     }
 
-    async comoponentDidMount() {
+    async componentDidMount() {
         const sign = this.props.match.params.sign
         const api = await Axios(`https://cors-anywhere.herokuapp.com/http://ohmanda.com/api/horoscope/${sign}`)
         this.setState({
-            sign: api.data
+            sign: api.data.sign,
+            horoscope: api.data.horoscope
         })
     }
 
     render() {
-        console.log(this.props)
-        console.log(this.state)
-    
+        // console.log(this.props)
+        // console.log(this.state)
+        const sign = this.state.sign
+        const horoscope = this.state.horoscope
+
         return(
             <div className="Profile">
-                <p>Placeholder</p>
+                <h2>{sign}</h2>
+                <p>{horoscope}</p>
             </div>
         )
     }
